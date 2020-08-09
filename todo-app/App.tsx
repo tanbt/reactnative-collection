f5463f21b912b65ba50c73e1fb3247c298ad8349
tanbui <trungtanbui@gmail.com>
2020-08-09 22:04:34 +0300

    Add an event handler

--- todo-app/App.tsx
+++ todo-app/App.tsx
@@ -1,34 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>Welcome to my Todo app!</Text>
-      <TextInput style={styles.input} value='Enter a task' />
+      <TextInput style={styles.input} placeholder="Enter a task"
+      onChangeText={text => onChangeText(text)}/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const onChangeText = (text: String) => {
+  console.log("Receive text: " + text);
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 5
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 5,
   },
   heading: {
     fontSize: 25,
-    marginBottom: 10
+    marginBottom: 10,
   },
   input: {
     borderWidth: 1,
-    borderColor: 'gray',
+    borderColor: "gray",
     height: 40,
-    alignSelf: 'stretch',
-    padding: 5
-  }
+    alignSelf: "stretch",
+    padding: 5,
+  },
 });
--- todo-app/README.md
+++ todo-app/README.md
@@ -7,7 +7,9 @@
 ## Plans
 * Build the most basic Todo App using core components
 * Replace core components by more-beautiful components
-* Use Redux to manage states https://medium.com/@unionproject88/react-native-todo-app-with-redux-1a612266151b
+* Use Redux to manage states
+https://medium.com/@unionproject88/react-native-todo-app-with-redux-1a612266151b
+https://dev.to/shivabhusal/step-to-step-guide-to-build-a-todo-app-with-react-and-redux-11pd
 * Add a second tab
 * The second tab is to display all data (from device storage) of the app
 * Send data to firebase storage or google cloud
