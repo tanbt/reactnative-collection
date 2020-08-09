2e4fa14469c1efc8018e2721521f911d8078f7ed
tanbui <trungtanbui@gmail.com>
2020-08-09 21:51:40 +0300

    Add TextInput and some styles

--- todo-app/App.tsx
+++ todo-app/App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.heading}>Welcome to my Todo app!</Text>
+      <TextInput style={styles.input} value='Enter a task' />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +18,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 5
   },
+  heading: {
+    fontSize: 25,
+    marginBottom: 10
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    height: 40,
+    alignSelf: 'stretch',
+    padding: 5
+  }
 });
---
+++ todo-app/README.md
@@ -0,0 +1,14 @@
+# My first react-native project as a Todo App
+
+## Run
+* npm ci
+* npm run web
+
+## Plans
+* Build the most basic Todo App using core components
+* Replace core components by more-beautiful components
+* Use Redux to manage states https://medium.com/@unionproject88/react-native-todo-app-with-redux-1a612266151b
+* Add a second tab
+* The second tab is to display all data (from device storage) of the app
+* Send data to firebase storage or google cloud
+* Authentication ?
