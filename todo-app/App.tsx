ede059903172f66ba6056b8c41e6572cf7f91054
tanbui <trungtanbui@gmail.com>
2020-08-14 12:52:07 +0300

    Navigation: add dependencies and navigate to another screen

--- todo-app/App.tsx
+++ todo-app/App.tsx
@@ -1,12 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import { StyleSheet, SafeAreaView, Button } from "react-native";
 import Header from "./src/navigation/Header";
 import TodoScreen from "./screens/TodoScreen";
+import DataScreen from "./screens/DataScreen";
+import { StackNavigationHelpers } from "@react-navigation/stack/lib/typescript/src/types";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }: {navigation: StackNavigationHelpers}) {
   const { container } = styles;
-
   return (
     <SafeAreaView style={container}>
       {/* todo change title based on screen */}
@@ -14,9 +19,29 @@ export default function App() {
 
       <TodoScreen />
 
-      <Button title="Go to Data Screen" onPress={() => {console.log("pressed")}} />
+      <Button
+        title="Go to Data Screen"
+        onPress={() => {
+          navigation.navigate("Data");
+        }}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen
+          name="Data"
+          component={DataScreen}
+          options={{ title: "Todo data" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- todo-app/screens/DataScreen.tsx
+++ todo-app/screens/DataScreen.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {View, Text, Button, TouchableOpacity} from 'react-native';
+import { StackNavigationHelpers } from '@react-navigation/stack/lib/typescript/src/types';
 
-export default function DataScreen() {
+export default function DataScreen({navigation} : {navigation: StackNavigationHelpers}) {
   return (
     <View>
       <Text>Hello from Data Screen.</Text>
-      <TouchableOpacity onPress={() => {console.log("touch")}}>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
         <Text>Go Home</Text>
         <Text>Go Home</Text>
       </TouchableOpacity>
