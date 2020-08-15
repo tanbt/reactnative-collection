2262a5a552acff86d3b9779e515253f404b12603
tanbui <trungtanbui@gmail.com>
2020-08-15 21:58:37 +0300

    Add dependencies and ask for recording permission

--- recorder-expo/App.tsx
+++ recorder-expo/App.tsx
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Audio } from "expo-av";
+import * as Permissions from "expo-permissions";
 
 export default function App() {
+  const [isAllowRecord, setAllowRecord] = useState(false);
+
+  const recordingSettings = JSON.parse(
+    JSON.stringify(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY)
+  );
+
+  // similar to componentDidMount and componentDidUpdate
+  useEffect(() => {
+    _askForPermissions();
+  });
+
+  async function _askForPermissions() {
+    const response = await Permissions.askAsync(Permissions.AUDIO_RECORDING);
+    setAllowRecord(response.status === "granted");
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+
+      <Button title="start record" onPress={} />
+      <Button title="stop record" onPress={} />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ recorder-expo/README.md
@@ -0,0 +1,7 @@
+
+## Install expo-av dependency
+https://github.com/expo/expo/tree/master/packages/expo-av
+```
+expo install expo-av
+```
+
