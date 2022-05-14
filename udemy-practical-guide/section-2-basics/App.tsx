402555460c01211ff3e9e5667c6f8c655c0a1dd7
Tan Bui <tan.bui@virta.global>
2022-05-14 19:00:22 +0300

    Simple interaction & useState

--- udemy-practical-guide/section-2-basics/App.tsx
+++ udemy-practical-guide/section-2-basics/App.tsx
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [goalInput, setGoalInput] = useState<string>("");
+  const [goals, setGoals] = useState<string[]>([]);
+
+  function handleAddGoal() {
+    setGoals([...goals, goalInput]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your goal..."
+          defaultValue={goalInput}
+          onChangeText={setGoalInput}
+        />
+        <Button title="Add Goal" onPress={handleAddGoal} />
+      </View>
+      <View style={styles.goalsContainer}>
+        {goals.map((goal) => (
+          <Text key={goal}>{goal}</Text>
+        ))}
+        {/* <FlatList data={goals} renderItem={(ri) => <Text>{ri.item}</Text>} /> */}
+      </View>
     </View>
   );
 }
@@ -13,8 +33,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 5,
+    paddingTop: 20,
+    flexDirection: "column",
+  },
+  inputContainer: {
+    flexDirection: "row",
+    paddingBottom: 10,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#DDDDDD",
+    alignItems: "center",
+  },
+  textInput: {
+    flex: 1,
+  },
+  goalsContainer: {
+    borderTopColor: "#DDDDDD",
   },
 });
