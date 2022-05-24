46a7d97bc6bc01efc3fc7b390f16c7cb9ceddd32
Tan Bui <tan.bui@virta.global>
2022-05-25 00:39:10 +0300

    Platform specific styling and files

--- udemy-practical-guide/section-3-guess-number-game/components/Title.tsx
+++ udemy-practical-guide/section-3-guess-number-game/components/Title.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text } from "react-native";
+import { Platform, StyleSheet, Text } from "react-native";
 
 export function Title({ children }: { children: any }) {
   return <Text style={s.title}>{children}</Text>;
@@ -6,11 +6,12 @@ export function Title({ children }: { children: any }) {
 
 const s = StyleSheet.create({
   title: {
-    fontFamily: 'open-sans-bold',
+    fontFamily: "open-sans-bold",
     fontSize: 24,
     color: "white",
     textAlign: "center",
-    borderWidth: 2,
+    // borderWidth: Platform.OS === "android" ? 2 : 0,
+    borderWidth: Platform.select({ ios: 0, android: 2, web: 1 }),
     borderColor: "white",
     padding: 12,
   },
---
+++ udemy-practical-guide/section-3-guess-number-game/util/constants.android.ts
@@ -0,0 +1,8 @@
+// Rename the file with postfix `*.android.ts` or `*.ios.ts` to use it for 
+// a specific platform. The import statement remains unchanged.
+
+export enum Colors {
+  primary500 = "#527206",
+  primary600 = "#640200",
+  accent500 = "#ddb500",
+}
