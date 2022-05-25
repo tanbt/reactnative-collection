dde533c438fc8cddf95fb7905fb846c1f6ad4d50
Tan Bui <tan.bui@virta.global>
2022-05-25 17:53:00 +0300

    Install reactnative navigation dependencies and first screen setup

--- udemy-practical-guide/sec-6-meals-app/App.tsx
+++ udemy-practical-guide/sec-6-meals-app/App.tsx
@@ -1,17 +1,25 @@
 import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { CategoriesScreen } from "./screens/CategoriesScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <StatusBar style="light" />
-      <CategoriesScreen />
-    </SafeAreaView>
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Categories">
+          <Stack.Screen name="Categories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
-const styles = StyleSheet.create({
+const s = StyleSheet.create({
   container: {
     flex: 1,
   },
