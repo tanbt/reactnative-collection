8cc3e73551aa546776ec1b71fe8f41d4b3da434c
Tan Bui <tan.bui@virta.global>
2022-05-26 10:56:24 +0300

    Add drawer icon

--- udemy-practical-guide/sec-6-meals-app/App.tsx
+++ udemy-practical-guide/sec-6-meals-app/App.tsx
@@ -5,6 +5,7 @@ import { CategoriesScreen } from "./screens/CategoriesScreen";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import { Ionicons } from "@expo/vector-icons";
 import { MealsOverview } from "./screens/MealsOverview";
 import { SCREENS } from "./util/Constants";
 import { MealDetail } from "./screens/MealDetail";
@@ -23,13 +24,36 @@ const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "#351401" },
+        headerTintColor: "white",
+        sceneContainerStyle: { backgroundColor: "#3f4f42" },
+        drawerContentStyle: { backgroundColor: "#351401" },
+        drawerInactiveTintColor: "white",
+        drawerActiveTintColor: "#351401",
+        drawerActiveBackgroundColor: "#e4baa1",
+      }}
+    >
       <Drawer.Screen
         name={SCREENS.Category}
         component={CategoriesScreen}
-        options={{ title: "Choose a category" }}
+        options={{
+          title: "Choose a category",
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" size={size} color={color} />
+          ),
+        }}
       />
-      <Drawer.Screen name={SCREENS.Favorite} component={FavoriteScreen} />
+      <Drawer.Screen
+        name={SCREENS.Favorite}
+        component={FavoriteScreen}
+        options={{
+          drawerIcon: ({ size, color }) => (
+            <Ionicons name="star" {...{ size, color }} />
+          ),
+        }}
+      />
     </Drawer.Navigator>
   );
 }
@@ -43,8 +67,8 @@ export default function App() {
           initialRouteName={SCREENS.Category}
           screenOptions={{
             headerStyle: { backgroundColor: "#351401" },
-            headerTintColor: "white",
             contentStyle: { backgroundColor: "#3f4f42" },
+            headerTintColor: "white",
           }}
         >
           <Stack.Screen
