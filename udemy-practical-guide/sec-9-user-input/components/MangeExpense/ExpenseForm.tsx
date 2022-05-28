bcc47510f2d84938cfa16e75ce7a23a44ba632c8
Tan Bui <tan.bui@virta.global>
2022-05-28 19:31:41 +0300

    Styling input and form

--- udemy-practical-guide/sec-9-user-input/components/MangeExpense/ExpenseForm.tsx
+++ udemy-practical-guide/sec-9-user-input/components/MangeExpense/ExpenseForm.tsx
@@ -1,26 +1,31 @@
-import { View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { Input } from "./Input";
 
 export function ExpenseForm() {
   function amountChangeHandler() {}
 
   return (
-    <View>
-      <Input
-        label="Amount"
-        textInputConfig={{
-          keyboardType: "decimal-pad",
-          onChangeText: amountChangeHandler,
-        }}
-      />
-      <Input
-        label="Date"
-        textInputConfig={{
-          placeholder: "YYYY-MM-DD",
-          maxLength: 10,
-          onChangeText: () => {},
-        }}
-      />
+    <View style={s.root}>
+      <Text style={s.title}>Your Expense</Text>
+      <View style={s.amountDateWrapper}>
+        <Input
+          label="Amount"
+          style={s.rowInput}
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: amountChangeHandler,
+          }}
+        />
+        <Input
+          label="Date"
+          style={s.rowInput}
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: () => {},
+          }}
+        />
+      </View>
       <Input
         label="Description"
         textInputConfig={{
@@ -32,3 +37,24 @@ export function ExpenseForm() {
     </View>
   );
 }
+
+const s = StyleSheet.create({
+  root: {
+    marginTop: 40,
+    marginBottom: 20,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+    marginVertical: 24,
+  },
+  amountDateWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+});
--- udemy-practical-guide/sec-9-user-input/components/MangeExpense/Input.tsx
+++ udemy-practical-guide/sec-9-user-input/components/MangeExpense/Input.tsx
@@ -1,16 +1,46 @@
-import { Text, TextInputProps, View } from "react-native";
+import { StyleSheet, Text, TextInputProps, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
+import { GlobalStyles } from "../../constants/styles";
 
 interface Props {
   label: string;
+  style?: any;
   textInputConfig?: TextInputProps;
 }
 
-export function Input({ label, textInputConfig }: Props) {
+export function Input({ label, style = {}, textInputConfig }: Props) {
+  const isMultiline = textInputConfig && textInputConfig.multiline;
+
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig} />
+    <View style={[s.container, style]}>
+      <Text style={s.label}>{label}</Text>
+      <TextInput
+        style={[s.input, isMultiline ? s.inputMultiline : null]}
+        {...textInputConfig}
+      />
     </View>
   );
 }
+
+const s = StyleSheet.create({
+  container: {
+    marginHorizontal: 4,
+    marginVertical: 8,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+    color: GlobalStyles.colors.primary800,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: "top", // recommended style
+  },
+});
