6f475be0775a86cf3612af4c374a041679703ea2
Tan Bui <tan.bui@virta.global>
2022-05-28 18:11:24 +0300

    Covert project to TS

--- udemy-practical-guide/sec-9-user-input/components/UI/Button.tsx
+++ udemy-practical-guide/sec-9-user-input/components/UI/Button.tsx
@@ -1,15 +1,15 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import { GlobalStyles } from '../../constants/styles';
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
-function Button({ children, onPress, mode, style }) {
+function Button({ children, onPress, mode = "", style }) {
   return (
     <View style={style}>
       <Pressable
         onPress={onPress}
         style={({ pressed }) => pressed && styles.pressed}
       >
-        <View style={[styles.button, mode === 'flat' && styles.flat]}>
-          <Text style={[styles.buttonText, mode === 'flat' && styles.flatText]}>
+        <View style={[styles.button, mode === "flat" && styles.flat]}>
+          <Text style={[styles.buttonText, mode === "flat" && styles.flatText]}>
             {children}
           </Text>
         </View>
@@ -27,11 +27,11 @@ const styles = StyleSheet.create({
     backgroundColor: GlobalStyles.colors.primary500,
   },
   flat: {
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
   },
   buttonText: {
-    color: 'white',
-    textAlign: 'center',
+    color: "white",
+    textAlign: "center",
   },
   flatText: {
     color: GlobalStyles.colors.primary200,
--- udemy-practical-guide/sec-9-user-input/store/expenses-context.tsx
+++ udemy-practical-guide/sec-9-user-input/store/expenses-context.tsx
@@ -1,59 +1,59 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer } from "react";
 
 const DUMMY_EXPENSES = [
   {
-    id: 'e1',
-    description: 'A pair of shoes',
+    id: "e1",
+    description: "A pair of shoes",
     amount: 59.99,
-    date: new Date('2021-12-19'),
+    date: new Date("2021-12-19"),
   },
   {
-    id: 'e2',
-    description: 'A pair of trousers',
+    id: "e2",
+    description: "A pair of trousers",
     amount: 89.29,
-    date: new Date('2022-01-05'),
+    date: new Date("2022-05-27"),
   },
   {
-    id: 'e3',
-    description: 'Some bananas',
+    id: "e3",
+    description: "Some bananas",
     amount: 5.99,
-    date: new Date('2021-12-01'),
+    date: new Date("2021-12-01"),
   },
   {
-    id: 'e4',
-    description: 'A book',
+    id: "e4",
+    description: "A book",
     amount: 14.99,
-    date: new Date('2022-02-19'),
+    date: new Date("2022-05-28"),
   },
   {
-    id: 'e5',
-    description: 'Another book',
+    id: "e5",
+    description: "Another book",
     amount: 18.59,
-    date: new Date('2022-02-18'),
+    date: new Date("2022-02-18"),
   },
   {
-    id: 'e6',
-    description: 'A pair of trousers',
+    id: "e6",
+    description: "A pair of trousers",
     amount: 89.29,
-    date: new Date('2022-01-05'),
+    date: new Date("2022-01-05"),
   },
   {
-    id: 'e7',
-    description: 'Some bananas',
+    id: "e7",
+    description: "Some bananas",
     amount: 5.99,
-    date: new Date('2021-12-01'),
+    date: new Date("2022-05-22"),
   },
   {
-    id: 'e8',
-    description: 'A book',
+    id: "e8",
+    description: "A book",
     amount: 14.99,
-    date: new Date('2022-02-19'),
+    date: new Date("2022-02-19"),
   },
   {
-    id: 'e9',
-    description: 'Another book',
+    id: "e9",
+    description: "Another book",
     amount: 18.59,
-    date: new Date('2022-02-18'),
+    date: new Date("2022-02-18"),
   },
 ];
 
@@ -66,10 +66,10 @@ export const ExpensesContext = createContext({
 
 function expensesReducer(state, action) {
   switch (action.type) {
-    case 'ADD':
+    case "ADD":
       const id = new Date().toString() + Math.random().toString();
       return [{ ...action.payload, id: id }, ...state];
-    case 'UPDATE':
+    case "UPDATE":
       const updatableExpenseIndex = state.findIndex(
         (expense) => expense.id === action.payload.id
       );
@@ -78,7 +78,7 @@ function expensesReducer(state, action) {
       const updatedExpenses = [...state];
       updatedExpenses[updatableExpenseIndex] = updatedItem;
       return updatedExpenses;
-    case 'DELETE':
+    case "DELETE":
       return state.filter((expense) => expense.id !== action.payload);
     default:
       return state;
@@ -89,15 +89,15 @@ function ExpensesContextProvider({ children }) {
   const [expensesState, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES);
 
   function addExpense(expenseData) {
-    dispatch({ type: 'ADD', payload: expenseData });
+    dispatch({ type: "ADD", payload: expenseData });
   }
 
   function deleteExpense(id) {
-    dispatch({ type: 'DELETE', payload: id });
+    dispatch({ type: "DELETE", payload: id });
   }
 
   function updateExpense(id, expenseData) {
-    dispatch({ type: 'UPDATE', payload: { id: id, data: expenseData } });
+    dispatch({ type: "UPDATE", payload: { id: id, data: expenseData } });
   }
 
   const value = {
