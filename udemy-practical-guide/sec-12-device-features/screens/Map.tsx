84b994906c5b2123b9e0c37f32b51d2d91755f71
Tan Bui <tan.bui@virta.global>
2022-05-30 00:45:59 +0300

    Get picked location

--- udemy-practical-guide/sec-12-device-features/screens/Map.tsx
+++ udemy-practical-guide/sec-12-device-features/screens/Map.tsx
@@ -1,9 +1,17 @@
+import { useNavigation } from "@react-navigation/native";
+import { useLayoutEffect } from "react";
+import { useCallback } from "react";
+import { useEffect } from "react";
 import { useState } from "react";
-import { StyleSheet } from "react-native";
+import { Alert, StyleSheet } from "react-native";
 import MapView, { LatLng, MapEvent, Marker, Region } from "react-native-maps";
+import { IconButton } from "../components/UI/IconButton";
 
 export function Map() {
-  const [selectedLocation, setSelectedLocation] = useState<LatLng>();
+  const navigation = useNavigation<any>();
+  const [selectedLocation, setSelectedLocation] = useState<
+    LatLng | undefined
+  >();
   const region: Region = {
     latitude: 63.102503,
     longitude: 21.6182102,
@@ -12,8 +20,34 @@ export function Map() {
   };
 
   function selectLocationHandler(event: MapEvent) {
-    setSelectedLocation(event.nativeEvent.coordinate);
+    setSelectedLocation({ ...event.nativeEvent.coordinate });
   }
+
+  useEffect(() => console.log(selectedLocation), [selectedLocation]);
+
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectedLocation) {
+      Alert.alert(
+        "Missing location",
+        "Please pick a location by tapping on the map!"
+      );
+      return;
+    }
+    navigation.navigate("AddPlace", { pickedLocation: selectedLocation });
+  }, [navigation, selectedLocation]);
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({ tintColor }: any) => (
+        <IconButton
+          icon="save"
+          onPress={savePickedLocationHandler}
+          size={24}
+          color={tintColor}
+        />
+      ),
+    });
+  }, [navigation, savePickedLocationHandler]);
 
   return (
     <MapView
