61f572cd8741ee90630eff6e7f1d65526b90f4e2
Tan Bui <tan.bui@virta.global>
2022-05-30 00:23:31 +0300

    Select a location on map

--- udemy-practical-guide/sec-12-device-features/screens/Map.tsx
+++ udemy-practical-guide/sec-12-device-features/screens/Map.tsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
-import MapView, { Marker, Region } from "react-native-maps";
+import MapView, { LatLng, MapEvent, Marker, Region } from "react-native-maps";
 
 export function Map() {
+  const [selectedLocation, setSelectedLocation] = useState<LatLng>();
   const region: Region = {
     latitude: 63.102503,
     longitude: 21.6182102,
     latitudeDelta: 0.05,
     longitudeDelta: 0.05,
   };
-  return <MapView style={s.map} initialRegion={region}></MapView>;
+
+  function selectLocationHandler(event: MapEvent) {
+    setSelectedLocation(event.nativeEvent.coordinate);
+  }
+
+  return (
+    <MapView
+      style={s.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker coordinate={selectedLocation} title="Picked location" />
+      )}
+    </MapView>
+  );
 }
 
 const s = StyleSheet.create({
