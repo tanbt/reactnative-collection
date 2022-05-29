3cfc702877090f3e0573f228f4053c7347788c61
Tan Bui <tan.bui@virta.global>
2022-05-29 19:49:20 +0300

    IOS - ask for camera permission and take photo

--- udemy-practical-guide/sec-12-device-features/components/places/ImagePicker.tsx
+++ udemy-practical-guide/sec-12-device-features/components/places/ImagePicker.tsx
@@ -1,8 +1,35 @@
-import { Button, View } from "react-native";
-import { launchCameraAsync } from "expo-image-picker";
+import { Alert, Button, View } from "react-native";
+import {
+  launchCameraAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
 
 export function ImagePicker() {
+  const [cameraPermissionStatus, requestPermission] = useCameraPermissions();
+
+  async function verifyPermission(): Promise<boolean> {
+    if (cameraPermissionStatus?.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionStatus?.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Missing permissions",
+        "Please grant camera permission to continue."
+      );
+      return false;
+    }
+    return true;
+  }
+
   async function takeImageHandler() {
+    const hasPerm = await verifyPermission();
+    if (!hasPerm) {
+      return;
+    }
+
     const image = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
