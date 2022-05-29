55dce4cde53d44294978551a731f4074022d51e8
Tan Bui <tan.bui@virta.global>
2022-05-29 20:00:09 +0300

    Image preview

--- udemy-practical-guide/sec-12-device-features/components/places/ImagePicker.tsx
+++ udemy-practical-guide/sec-12-device-features/components/places/ImagePicker.tsx
@@ -1,11 +1,15 @@
-import { Alert, Button, View } from "react-native";
+import { Alert, Button, Image, StyleSheet, Text, View } from "react-native";
 import {
   launchCameraAsync,
   useCameraPermissions,
   PermissionStatus,
+  ImageInfo,
 } from "expo-image-picker";
+import { useState } from "react";
+import { Colors } from "../../constants/Colors";
 
 export function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState<string>();
   const [cameraPermissionStatus, requestPermission] = useCameraPermissions();
 
   async function verifyPermission(): Promise<boolean> {
@@ -35,13 +39,32 @@ export function ImagePicker() {
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
+    setPickedImage((image as ImageInfo).uri);
   }
 
   return (
     <View>
-      <View></View>
+      <View style={s.imagePreview}>
+        {!pickedImage && <Text>No image taken yet.</Text>}
+        {pickedImage && <Image style={s.image} source={{ uri: pickedImage }} />}
+      </View>
       <Button title="Take Image" onPress={takeImageHandler} />
     </View>
   );
 }
+
+const s = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
