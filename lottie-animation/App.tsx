2c8fe8aa4905e4235cdaa38cbdd10a40a674fde2
Tan Bui <trungtanbui@gmail.com>
2022-12-03 17:43:53 +0200

    Lottie background

--- lottie-animation/App.tsx
+++ lottie-animation/App.tsx
@@ -1,14 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import LottieAnimation from "./LottieAnimation";
+import LottieBackground from "./LottieBackground";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <LottieAnimation />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.tsx to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    //   <LottieAnimation />
+    // </View>
+    <LottieBackground />
   );
 }
 
---
+++ lottie-animation/LottieBackground.tsx
@@ -0,0 +1,51 @@
+import React, { useRef, useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import LottieView from "lottie-react-native";
+
+export default function LottieBackground() {
+  const [name, setName] = useState<string>("");
+  return (
+    <SafeAreaView>
+      <View style={styles.content}>
+        <Text>Enter your name</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(text) => setName(text)}
+        />
+        {!!name && <Text>Hello, {name}!</Text>}
+      </View>
+      <LottieView
+        autoPlay
+        style={styles.lottieView}
+        source={require("./assets/PoliteChicky.json")}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  lottieView: {
+    flex: 1,
+    backgroundColor: "#ffe4b3",
+  },
+  content: {
+    zIndex: 9999,
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderRadius: 5,
+    height: 50,
+    padding: 10,
+    width: "50%",
+  },
+});
