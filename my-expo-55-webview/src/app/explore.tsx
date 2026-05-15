c026174d718656a56de30f40b67de0e7b4ca1316
Tony Bui <tony.bui@aiven.io>
2026-05-15 10:56:27 +0300

    Run when first load

--- my-expo-55-webview/src/app/explore.tsx
+++ my-expo-55-webview/src/app/explore.tsx
@@ -5,6 +5,12 @@ import { WebView } from 'react-native-webview';
 
 const myHtmlFile = require('../../assets/local-site.html');
 
+const runJSOnWebview = `
+      document.body.style.backgroundColor = 'red';
+      setTimeout(function() { window.alert('This will run once the page is loaded.') }, 2000);
+      true; // note: this is required, or you'll sometimes get silent failures
+    `;
+
 
 export default function ExploreScreen() {
   const theme = useTheme();
@@ -12,7 +18,8 @@ export default function ExploreScreen() {
   return (
     <SafeAreaView style={[styles.root, { backgroundColor: theme.background }]}>
       {/* <WebView style={styles.container} source={{ uri: 'https://expo.dev' }} /> */}
-      <WebView style={styles.container} source={myHtmlFile}  />
+      <WebView style={styles.container} source={myHtmlFile} onMessage={(event) => { }}
+        injectedJavaScript={runJSOnWebview} />
     </SafeAreaView>
   );
 }
